67680ac5b77de79d0efa6be554cd1575c50f556c
nie.chao <nie.chao@advantech.com.cn>
2024-07-19 11:21:40 +0800

    指定端口

--- MyZhiHuAPI/Models/Auth.cs
+++ MyZhiHuAPI/Models/Auth.cs
@@ -7,6 +7,6 @@ public class Auth
 
 public class AuthLogin
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
 }
--- MyZhiHuAPI/Models/User.cs
+++ MyZhiHuAPI/Models/User.cs
@@ -6,17 +6,17 @@ namespace MyZhiHuAPI.Models;
 public class User
 {
     public int Id { get; set; }
-    public string Username { get; set; }
+    public string? Username { get; set; }
     public string? Password { get; set; }
-    public string Nickname { get; set; }
+    public string? Nickname { get; set; }
     public string? Email { get; set; }
     public string? Phone { get; set; }
-    public int[] Questions { get; set; }
-    public int[] Answers { get; set; }
-    public int[] Commits { get; set; }
-    public int[] Remarks { get; set; }
-    public int[] Watching_People { get; set; }
-    public int[] Watching_Question { get; set; }
+    public int[]? Questions { get; set; }
+    public int[]? Answers { get; set; }
+    public int[]? Commits { get; set; }
+    public int[]? Remarks { get; set; }
+    public int[]? Watching_People { get; set; }
+    public int[]? Watching_Question { get; set; }
 
     public override string ToString()
     {
@@ -38,11 +38,11 @@ public class User
 
 public class UserRegister
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Nickname { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Nickname { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
 }
 
 public class UserInfo
